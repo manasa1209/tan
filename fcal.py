0efcde1e265c9fa9fa5bbf99c6cb0345571d6541
smnavada <smnavada@gmail.com>
2023-11-03 14:28:08 +0530

    first

---
+++ fcal.py
@@ -0,0 +1,19 @@
+"""
+This is a Python script that does the simple operations of the calculator.
+"""
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+    
+    def subtract(self, x, y):
+        return x - y
+    
+    def multiply(self, x, y):
+        return x * y
+    
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Division by zero is not allowed")
+        return x / y
+
